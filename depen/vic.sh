27e2507051454b8c7743cb7030dcec8a0c08f002
th3_pr3d4t0r <55555800+tony23x@users.noreply.github.com>
2021-04-10 14:50:01 -0500

    Delete vic.sh

--- depen/vic.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-curl -s -N http://127.0.0.1:4040/api/tunnels | grep '{"name":"vic (http)","uri":"/api/tunnels/vic%20%28http%29","public_url":"http://[0-9a-z]*\.ngrok.io' -oh | tr '"' ' ' | awk '{print $13}' > vic.txt
