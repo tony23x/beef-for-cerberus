fc41fa7708ed3526c50496470dbf68ba693f180b
th3_pr3d4t0r <55555800+tony23x@users.noreply.github.com>
2021-04-11 10:25:32 -0500

    Update vic.sh

--- depen/vic.sh
+++ depen/vic.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-
-curl -s -N http://127.0.0.1:4040/api/tunnels | grep '{"name":"vic (http)","uri":"/api/tunnels/vic%20%28http%29","public_url":"http://[0-9a-z]*\.ngrok.io' -oh | tr '"' ' ' | awk '{print $13}' > vic.txt
-curl -s -N http://127.0.0.1:4040/api/tunnels | grep '{"name":"vic (http)","uri":"/api/tunnels/vic+%28http%29","public_url":"http://' | tr ':' ' ' | tr -d '"' | tr ',' ' ' | tr '/' ' ' | grep 'http' | awk '{print $119}' > vic.txt
+curl -s -N http://127.0.0.1:4040/api/tunnels | tr '"' ' ' | tr '}' '\n' | grep ',{ name : vic , uri : /api/tunnels/vic , public_url : ' | awk '{print $12}' > vic.txt
