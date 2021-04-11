a48b972450b5dbfea69abebb26b3ec2f8d3bb5cd
th3_pr3d4t0r <55555800+tony23x@users.noreply.github.com>
2021-04-11 10:37:31 -0500

    Update panel.sh

--- depen/panel.sh
+++ depen/panel.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-
-curl -s -N http://127.0.0.1:4040/api/tunnels | grep '{"name":"beef (http)","uri":"/api/tunnels/beef%20%28http%29","public_url":"http://[0-9a-z]*\.ngrok.io' -oh | tr '"' ' ' | awk '{print $13}' > panel.txt
-curl -s -N http://127.0.0.1:4040/api/tunnels | grep '{"name":"beef (http)","uri":"/api/tunnels/beef+%28http%29","public_url":"http://' | tr ':' ' ' | tr -d '"' | tr ',' ' ' | tr '/' ' ' | grep 'http' | awk '{print $11}' > panel.txt
+curl -s -N http://127.0.0.1:4040/api/tunnels | tr '"' ' ' | tr '}' '\n' | grep ',{ name : beef , uri : /api/tunnels/beef , public_url : ' | awk '{print $12}' > panel.txt
